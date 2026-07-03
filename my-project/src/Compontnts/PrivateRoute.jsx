d27b1e53a903e70e24305d173a7eedce0e3df83b
Saqlain-313 <saqlainmalik@maxifysolution.com>
2026-07-03 13:10:20 +0530

    by malik

--- my-project/src/Compontnts/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-const PrivateRoute = () => {
-  const isAuth = localStorage.getItem("token");   // change to your auth logic
-
-  return isAuth ? <Outlet /> : <Navigate to="/" />;
-};
-
-export default PrivateRoute;
